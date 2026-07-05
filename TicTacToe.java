2f6d84757ba4fd0b3504d887bcf183f2ea9cd6b7
alekekex <alexander_khan@dlsu.edu.ph>
2026-07-06 00:01:56 +0800

    added basic ai mode

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class TicTacToe {
     private static final int MAX_PLAYERS = 2;
@@ -19,24 +20,46 @@ public class TicTacToe {
     }
 
     public void playGame(Scanner sc) {
+        Random rnd = new Random();
         b.initializeBoard();
 
         while(!isGameOver) {
             System.out.println();
-            System.out.println(p[playerIdx].getName() + "\'s turn (" + p[playerIdx].getM().getSymbol() + ")");
             b.displayBoard();
-            boolean isAvailable = false;
+            System.out.println(p[playerIdx].getName() + "\'s turn (" + p[playerIdx].getM().getSymbol() + ")");
+            boolean isValid = false;
+            int row, column;
 
-            while(!isAvailable) {
-                System.out.println("Enter row (Range 1-3)");
-                int row = Input.getIntChoice(sc, MIN, MAX);
-                System.out.println("Enter column (Range 1-3)");
-                int column = Input.getIntChoice(sc, MIN, MAX);
-                isAvailable = b.placeMarker(p[playerIdx].getM(), row, column);
+            if(p[playerIdx].isAI()) {
+                System.out.println(p[playerIdx].getName() + " is thinking...");
 
-                if(!isAvailable)
+                try {
+                    Thread.sleep(1500);
+                } catch(InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+
+            while(!isValid) {
+                if(p[playerIdx].isAI()) {
+                    row = rnd.nextInt(MAX) + MIN;
+                    column = rnd.nextInt(MAX) + MIN;
+                }
+                else {
+                    System.out.println("Enter row (Range 1-3)");
+                    row = Input.getIntChoice(sc, MIN, MAX);
+                    System.out.println("Enter column (Range 1-3)");
+                    column = Input.getIntChoice(sc, MIN, MAX);
+                }
+
+                isValid = b.placeMarker(p[playerIdx].getM(), row, column);
+
+                if(isValid)
+                    System.out.println("Move: (" + row + "," + column + ")");
+                else if (!(p[playerIdx].isAI()))
                     System.out.println("That spot is already taken! Try again.");
             }
+
 
             if(b.isWinner(p[playerIdx].getM()) || b.isBoardFull())
                 isGameOver = true;

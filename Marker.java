0837510a5e74d75c38a883180061433dfb599263
alekekex <alexander_khan@dlsu.edu.ph>
2026-07-05 13:09:43 +0800

    added constructor and getter

--- Marker.java
+++ Marker.java
@@ -1,3 +1,11 @@
 public class Marker {
+    private char symbol;
 
+    public Marker(char symbol) {
+        this.symbol = symbol;
+    }
+
+    public char getSymbol() {
+        return this.symbol;
+    }
 }
